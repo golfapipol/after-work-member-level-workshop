7a6726e606533c93c556c26d655077c7b4a2e350
Witsarut Laodara <ujumg@hotmail.com>
2018-08-08 20:11:03 +0700

    [Deleted]:  filtertransactionbyspending.go และ filtertransactionbyspending_test.go

--- src/memberlevel/memberlevel.go
+++ src/memberlevel/memberlevel.go
@@ -33,3 +33,23 @@ func CheckUserInRankByID(userId string) bool {
 	}
 	return false
 }
+
+type Transaction struct {
+	product string
+	price float64
+	date string
+}
+type Transactions []Transaction
+const price = 1000
+func filterTranscationBySpending(transactions Transactions) (Transactions) {
+
+	filterTransactions := Transactions{}
+	for i:=0; i< len(transactions);i++ {
+		if transactions[i].price > price {
+			//filterTransactions[i] transaction:=	transactions[i]
+			filterTransactions = append(filterTransactions, transactions[i])
+		}
+	}
+	
+	return filterTransactions
+}
