ebde596bed555be6a6f8cc0f03c7cb02a4e1654b
ploy <chonnikan.t@kkumail.com>
2018-08-08 20:03:12 +0700

    [Deleted]:ไฟล์ที่ไม่ได้ใช้แล้ว

--- src/memberlevel/checkspending.go
+++
@@ -1,8 +0,0 @@
-package memberlevel
-
-func CheckSpending(spending int) bool {
-	if spending > 1000 {
-		return true
-	}
-	return false
-}
--- src/memberlevel/checkuserinrankbyid.go
+++
@@ -1,9 +0,0 @@
-package memberlevel
-
-func CheckUserInRankByID(userId string) bool {
-id := "006"
-if userId ==id{
-	return true
-}
-   return false
-}
