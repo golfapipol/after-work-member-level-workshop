5c566bf8ac82bf82b8019c1a4f7d09a8ad0dc2d3
ploy <chonnikan.t@kkumail.com>
2018-08-08 20:03:56 +0700

    [Created]: สร้างไฟล์ร้อยการทำงานทั้งหมด

---
+++ src/memberlevel/memberlevel.go
@@ -0,0 +1,35 @@
+package memberlevel
+
+const orderCountCondition = 8
+const monthCondition = 6
+
+func CheckCountOrder(order int, month int) bool {
+	if CheckOrderPerMonth(month) {
+		if order >= orderCountCondition {
+			return true
+		}
+	}
+	return false
+}
+
+func CheckOrderPerMonth(month int) bool {
+	if month <= monthCondition {
+		return true
+	}
+	return false
+}
+
+func CheckSpending(spending int) bool {
+	if spending > 1000 {
+		return true
+	}
+	return false
+}
+
+func CheckUserInRankByID(userId string) bool {
+	id := "006"
+	if userId == id {
+		return true
+	}
+	return false
+}
