5bc24e532c0e526708008a3d113fbc6763a23177
GJ-Chokanan <chokanan@goingjesse.com>
2018-08-08 19:33:57 +0700

    [Added]: add function CheckOrderPerMonth

--- src/memberlevel/checkcountorder_test.go
+++ src/memberlevel/checkcountorder_test.go
@@ -5,11 +5,12 @@ import "testing"
 func Test_CheckCountOrder_Input_8_should_Be_True(t *testing.T) {
 	expected := true
 	order := 8
+	month := 6
 
-	actualResult := CheckCountOrder(order)
+	actualResult := CheckCountOrder(order,month)
 
 	if expected != actualResult {
-		t.Errorf("Expected %v but got it %v", expected, order)
+		t.Errorf("Expected %v but got it %v", expected, actualResult)
 	}
 
 }
@@ -17,11 +18,39 @@ func Test_CheckCountOrder_Input_8_should_Be_True(t *testing.T) {
 func Test_CheckCountOrder_Input_8_should_Be_False(t *testing.T) {
 	expected := false
 	order := 7
+	month := 7
 
-	actualResult := CheckCountOrder(order)
+	actualResult := CheckCountOrder(order,month)
 
 	if expected != actualResult {
-		t.Errorf("Expected %v but got it %v", expected, order)
+		t.Errorf("Expected %v but got it %v", expected, actualResult)
 	}
 
 }
+
+
+func Test_CheckOrderPerMonth_Input_6_should_Be_True(t *testing.T) {
+	expected := true
+	month := 6
+
+	actualResult := CheckOrderPerMonth(month)
+
+	if expected != actualResult {
+		t.Errorf("Expected %v but got it %v", expected, month)
+	}
+
+}
+
+func Test_CheckOrderPerMonth_Input_7_should_Be_False(t *testing.T) {
+	expected := false
+	month := 7
+
+	actualResult := CheckOrderPerMonth(month)
+
+	if expected != actualResult {
+		t.Errorf("Expected %v but got it %v", expected, month)
+	}
+
+}
+
+
