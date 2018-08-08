fa2bdbb57dd4f8c842855409cd7ffd7596fe18a5
somzai <pornpong@longkongstudio.com>
2018-08-08 19:40:23 +0700

    [Edited]: เปลี่ยนค่า Expected = false

--- src/memberlevel/checkspending_test.go
+++ src/memberlevel/checkspending_test.go
@@ -15,20 +15,9 @@ func Test_CheckSpending_Input_1200_Should_Be_True(t *testing.T) {
 	}
 }
 
-func Test_CheckSpending_Input_2000_Should_Be_False(t *testing.T) {
-	spending := 2000
-	expected := true
-
-	actual := CheckSpending(spending)
-
-	if expected != actual {
-		t.Errorf("Expected is %v but got %v", expected, actual)
-	}
-}
-
-func Test_CheckSpending_Input_1001_Should_Be_False(t *testing.T) {
-	spending := 1001
-	expected := true
+func Test_CheckSpending_Input_800_Should_Be_False(t *testing.T) {
+	spending := 800
+	expected := false
 
 	actual := CheckSpending(spending)
 
