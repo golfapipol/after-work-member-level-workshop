accfb8a2ebb6f77460937b70eb44c47611558512
PP10969 <putsareeparn@gmail.com>
2018-08-08 19:55:25 +0700

    [Edited]: edit file name and function

--- src/memberlevel/getfreepoint_test.go
+++ src/memberlevel/getfreepoint_test.go
@@ -1,4 +1,4 @@
-package memberlevel_test
+package memberlevel
 
 import (
 	"testing"
@@ -8,7 +8,7 @@ func Test_GetFreePoint_Input_Platinum_Should_Be_300(t *testing.T) {
 	expected := 300
 	level := "Platinum"
 
-	actualResult := CheckLevel(level)
+	actualResult := GetFreePoint(level)
 
 	if expected != actualResult {
 		t.Errorf("expected %d but got it %d", expected, actualResult)
@@ -19,7 +19,7 @@ func Test_GetFreePoint_Input_Gold_Should_Be_100(t *testing.T) {
 	expected := 100
 	level := "Gold"
 
-	actualResult := CheckLevel(level)
+	actualResult := GetFreePoint(level)
 
 	if expected != actualResult {
 		t.Errorf("expected %d but got it %d", expected, actualResult)
@@ -30,7 +30,7 @@ func Test_GetFreePoint_Input_A_Should_Be_0(t *testing.T) {
 	expected := 0
 	level := "A"
 
-	actualResult := CheckLevel(level)
+	actualResult := GetFreePoint(level)
 
 	if expected != actualResult {
 		t.Errorf("expected %d but got it %d", expected, actualResult)
